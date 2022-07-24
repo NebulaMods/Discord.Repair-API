05484493459c794c47ccb5ce056ef07e3fa16a7f
nebula <nebula@nebulamods.ca>
2022-07-24 19:17:53 -0300

    Added sticker model

---
+++ Database/Models/Backup Models/Sticker.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestoreCord.Database.Models.BackupModels;
+
+/// <summary>
+/// 
+/// </summary>
+public record Sticker
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Key]
+    public Guid key { get; init; } = new();
+}
